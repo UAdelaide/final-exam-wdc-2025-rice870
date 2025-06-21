0bb348fc65b48ed01932b711136f15ea01c2a04b
Isaac <isaac.rice66@gmail.com>
2025-06-21 06:20:54 +0000

    [automatic commit] 2025-06-21 06:20:54 UTC 159.196.49.152

--- part2/app.js
+++ part2/app.js
@@ -23,5 +23,14 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
+app.get('/api/dogs', async (req, res) => {
+  try {
+    const [dogs] = await db.execute(`SELECT Dogs.name AS dog_name, Dogs.size AS size, Users.username AS owner_username FROM Dogs JOIN Users on Dogs.owner_id=Users.user_id`);
+    res.json(dogs);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch dogs' });
+  }
+});
+
 // Export the app instead of listening here
 module.exports = app;
