e898186addabc4329b4a2fe9016b3aa001ae6510
Isaac <isaac.rice66@gmail.com>
2025-06-21 04:14:15 +0000

    [automatic commit] 2025-06-21 04:14:14 UTC 159.196.49.152

--- part1/app.js
+++ part1/app.js
@@ -107,7 +107,7 @@ let db;
 
 app.get('/api/dogs', async (req, res) => {
   try {
-    const [dogs] = await db.execute('SELECT Dogs.name AS dog_name, Dogs.size AS size, ');
+    const [dogs] = await db.execute('SELECT Dogs.name AS dog_name, Dogs.size AS size, (SELECT username FROM Users WHERE ');
     res.json(books);
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch books' });
