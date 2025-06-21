40c6f3c3d845ebef246d1bd8c5fc2027b18601a9
Isaac <isaac.rice66@gmail.com>
2025-06-21 07:13:06 +0000

    [automatic commit] 2025-06-21 07:13:06 UTC 159.196.49.152

--- part2/app.js
+++ part2/app.js
@@ -14,6 +14,7 @@ app.use(session({
     saveUninitialized: false,
     cookie: { secure: false }
 }));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.urlencoded({ extended: true }));
