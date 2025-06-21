d536852842f707993c399263dd82df67576d3cfd
Isaac <isaac.rice66@gmail.com>
2025-06-21 05:39:05 +0000

    [automatic commit] 2025-06-21 05:39:05 UTC 159.196.49.152

--- part2/app.js
+++ part2/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
-const session = requre('express-session')
+const session = require('express-session')
 
 app.use(session({
     secret: 'secretpassword',
