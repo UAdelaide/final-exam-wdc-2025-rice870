70f4a9c6855cc2f0721ef876fc9b842eb350366c
Isaac <isaac.rice66@gmail.com>
2025-06-21 05:25:14 +0000

    [automatic commit] 2025-06-21 05:25:14 UTC 159.196.49.152

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -37,18 +37,18 @@ router.get('/me', (req, res) => {
 
 // POST login (smarty version)
 router.post('/login', async (req, res) => {
-  const { email, password } = req.body;
+  const { username, password } = req.body;
 
   try {
     const [rows] = await db.query(`
       SELECT user_id, username, role FROM Users
-      WHERE email = ? AND password_hash = ?
+      WHERE username = ? AND password_hash = ?
     `, [email, password]);
 
     if (rows.length === 0) {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
-    
+
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
