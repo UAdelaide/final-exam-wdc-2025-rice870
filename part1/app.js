092249aaad707e993d47c7a91b6760ff6e6342de
Isaac <isaac.rice66@gmail.com>
2025-06-21 04:37:46 +0000

    [automatic commit] 2025-06-21 04:37:46 UTC 159.196.49.152

--- part1/app.js
+++ part1/app.js
@@ -149,6 +149,7 @@ app.get('/api/walkers/summary', async (req, res) => {
         WalkApplications.request_id = WalkRequests.request_id
         AND WalkApplications.walker_id = Users.user_id
         AND WalkApplications.status = 'accepted')
+      LEFT JOIN 
       `);
     res.json(books);
   } catch (err) {
