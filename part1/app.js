627ed711ade237fe49fe2a4befa01408d1bb4c5b
Isaac <isaac.rice66@gmail.com>
2025-06-21 04:25:41 +0000

    [automatic commit] 2025-06-21 04:25:41 UTC 159.196.49.152

--- part1/app.js
+++ part1/app.js
@@ -136,6 +136,28 @@ app.get('/api/walkrequests/open', async (req, res) => {
   }
 });
 
+app.get('/api/walkers/open', async (req, res) => {
+  try {
+    const [dogs] = await db.execute(`SELECT
+      WalkRequests.request_id,
+      Dogs.name AS dog_name,
+      WalkRequests.requested_time,
+      WalkRequests.duration_minutes,
+      WalkRequests.location,
+      Users.username AS owner_username
+
+      FROM WalkRequests
+      JOIN Dogs on Dogs.dog_id = WalkRequests.dog_id
+      JOIN Users on Users.user_id = Dogs.owner_id
+
+      WHERE WalkRequests.status = 'open'
+      `);
+    res.json(books);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch books' });
+  }
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 module.exports = app;
