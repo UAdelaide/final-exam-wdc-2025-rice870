6d2eb2f7f92a996dfa9681900132ddb5c13e5722
Isaac <isaac.rice66@gmail.com>
2025-06-21 04:23:36 +0000

    [automatic commit] 2025-06-21 04:23:36 UTC 159.196.49.152

--- part1/app.js
+++ part1/app.js
@@ -124,10 +124,10 @@ app.get('/api/walkrequests/open', async (req, res) => {
       WalkRequests.location,
       Users.username AS owner_username
 
-      WHERE W
-
       JOIN Dogs on Dogs.dog_id = WalkRequests.dog_id
       JOIN Users on Users.user_id = Dogs.owner_id
+
+      WHERE WalkRequests.status = 'open'
       `);
     res.json(books);
   } catch (err) {
