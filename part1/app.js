db7f1cdf778ba9e6f5bbbaf938317a571d68ecaf
Isaac <isaac.rice66@gmail.com>
2025-06-21 04:03:12 +0000

    [automatic commit] 2025-06-21 04:03:12 UTC 159.196.49.152

--- part1/app.js
+++ part1/app.js
@@ -19,7 +19,7 @@ let db;
     const connection = await mysql.createConnection({
       host: 'localhost',
       user: 'root',
-      password: '' // Set your MySQL root password
+      password: 'root' // Set your MySQL root password
     });
 
     // Create the database if it doesn't exist
